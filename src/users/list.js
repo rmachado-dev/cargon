5642601d3becf4c48bb6f635d5cf8c7b933ba39e
Rogerio <rogerio.gordo@aparecida.go.gov.br>
2022-01-26 16:54:19 -0300

    listagem de usuarios

--- src/users/list.js
+++ src/users/list.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 
 function List() {
   return (
@@ -7,21 +7,28 @@ function List() {
       <h2>Lista dos Usuários</h2>
       <Table striped bordered hover>
         <thead>
-          <tr>
+          <tr className="text-center">
             <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Gênero</th>
+            <th>Status</th>
+            <th>Ações</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>1</td>
             <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>nome@email.com</td>
+            <td>Masculino</td>
+            <td>Ativo</td>
+            <td className="text-center">
+               <Button variant="outline-primary" size="sm" className="m-1">+info</Button>
+               <Button variant="outline-warning" size="sm" className="m-1">editar</Button>
+               <Button variant="outline-dark" size="sm" className="m-1">deletar</Button>
+            </td>
           </tr>
-          
         </tbody>
       </Table>
     </>
