9a083a43d707a3ee8196c1786396a18a3920e31d
Rogerio Machado <webmaster@rogeriomachado.com.br>
2022-01-27 19:17:06 -0300

    useeffect e usestate para lista de usuarios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import List from "./users/list";
+import api from './services/api';
 
 function App() {
+  const [lista, setLista] = useState([]);
+  
+  useEffect(() => { 
+    api.get("/users?page=1")
+      .then((response) => setLista(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+    }, []);
+
   return (
     <>
       <Container>
         <Row>
           <Col>
             <h1>Processo Seletivo - ReactJS</h1>
-            <List />
+            <List usuarios={lista.data} />
           </Col>
         </Row>
-        
       </Container>
     </>
   );
