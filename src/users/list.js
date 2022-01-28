8c100480dcb7025c41c86ef2e4de8ae08d1feda9
Rogerio <rogerio.gordo@aparecida.go.gov.br>
2022-01-28 13:25:53 -0300

    ajustes e estilos

--- src/users/list.js
+++ src/users/list.js
@@ -4,20 +4,19 @@ import { Button, Table } from "react-bootstrap";
 function List({ usuarios }) {
   return (
     <>
-      <h2>Lista dos Usuários</h2>
-
+      <h1 className="py-5">Lista dos Usuários</h1>
       <Table striped bordered hover responsive>
         <thead>
           <tr className="text-center">
-            <th>#</th>
+            <th>ID</th>
             <th>Nome</th>
             <th>E-mail</th>
             <th>Gênero</th>
             <th>Status</th>
-            <th>Ações</th>
+            <th className="w-25">Ações</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="text-center">
           {usuarios && usuarios.length && usuarios.map((usuario, index) => (
             <tr key={usuario.id}>
               <td>{usuario.id}</td>
@@ -25,11 +24,11 @@ function List({ usuarios }) {
               <td>{usuario.email}</td>
               <td>{usuario.gender}</td>
               <td>{usuario.status}</td>
-              <td className="text-center">
+              <td>
                 <Button variant="outline-dark" size="sm" className="m-1">
                   +info
                 </Button>
-                <Button variant="outline-primary" size="sm" className="m-1">
+                <Button variant="outline-dark" size="sm" className="m-1">
                   editar
                 </Button>
                 <Button variant="outline-danger" size="sm" className="m-1">
