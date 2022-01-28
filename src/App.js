becf3444bc173f279e867af05f94fd3f0a143a29
Rogerio <rogerio.gordo@aparecida.go.gov.br>
2022-01-28 16:20:29 -0300

    componente cadastro

--- src/App.js
+++ src/App.js
@@ -2,39 +2,44 @@ import React, { useEffect, useState } from "react";
 import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 import List from "./users/list";
 import api from "./services/api";
+import Cadastro from "./users/add";
 
 function App() {
   const [lista, setLista] = useState([]);
-  
-  useEffect(() => { 
-    api.get("/users?page=1")
+
+  useEffect(() => {
+    api
+      .get("/users?page=1")
       .then((response) => setLista(response.data))
       .catch((err) => {
         console.error("ops! ocorreu um erro" + err);
       });
-    }, []);
+  }, []);
 
   return (
     <>
-    
-      <Navbar bg="light" variant="light">
+      <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">ReactJS</Navbar.Brand>
-        <Nav>
-          <Nav.Link href="/">Lista dos Usuários</Nav.Link>
-          <Nav.Link href="/cadastro">Cadastro</Nav.Link>
-        </Nav>
+          <Navbar.Brand href="#home">ReactJS</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/">Lista dos Usuários</Nav.Link>
+            <Nav.Link href="/cadastro">Cadastro</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
 
       <Container>
         <Row>
           <Col>
-            <List usuarios={lista.data} />          
+            <Cadastro />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <List usuarios={lista.data} />
           </Col>
         </Row>
       </Container>
-
     </>
   );
 }
