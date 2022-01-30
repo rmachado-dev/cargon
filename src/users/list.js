0a4caca7488d08a2ea7e94154788739838fe6bf3
Rogerio Machado <webmaster@rogeriomachado.com.br>
2022-01-29 23:09:23 -0300

    ajustes na lista

--- src/users/list.js
+++ src/users/list.js
@@ -4,42 +4,41 @@ import { Button, Table } from "react-bootstrap";
 function List({ usuarios }) {
   return (
     <>
-      <h2>Lista dos Usuários</h2>
-
+      <h1 className="py-5">Lista dos Usuários</h1>
       <Table striped bordered hover responsive>
         <thead>
           <tr className="text-center">
-            <th>#</th>
+            <th>ID</th>
             <th>Nome</th>
             <th>E-mail</th>
             <th>Gênero</th>
             <th>Status</th>
-            <th>Ações</th>
+            <th className="w-25">Ações</th>
           </tr>
         </thead>
-        <tbody>
-          {usuarios && usuarios.length && usuarios.map((usuario, index) => (
-            <tr key={usuario.id}>
-              <td>{usuario.id}</td>
-              <td>{usuario.name}</td>
-              <td>{usuario.email}</td>
-              <td>{usuario.gender}</td>
-              <td>{usuario.status}</td>
-              <td className="text-center">
-                <Button variant="outline-dark" size="sm" className="m-1">
-                  +info
-                </Button>
-                <Button variant="outline-primary" size="sm" className="m-1">
-                  editar
-                </Button>
-                <Button variant="outline-danger" size="sm" className="m-1">
-                  deletar
-                </Button>
-              </td>
-            </tr>
-          ))}
-
-          
+        <tbody className="text-center">
+          {usuarios &&
+            usuarios.length &&
+            usuarios.map((usuario, index) => (
+              <tr key={usuario.id}>
+                <td>{usuario.id}</td>
+                <td>{usuario.name}</td>
+                <td>{usuario.email}</td>
+                <td>{usuario.gender}</td>
+                <td>{usuario.status}</td>
+                <td>
+                  <Button variant="outline-dark" size="sm" className="m-1">
+                    +info
+                  </Button>
+                  <Button variant="outline-dark" size="sm" className="m-1">
+                    editar
+                  </Button>
+                  <Button variant="outline-danger" size="sm" className="m-1">
+                    deletar
+                  </Button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </Table>
     </>
