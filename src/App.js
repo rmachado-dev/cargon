e1d44ce3e992da6e60b61ed6d0786d22ae181ae6
Rogerio <rogerio.gordo@aparecida.go.gov.br>
2022-01-31 16:51:40 -0300

    atualização do componente

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
-import api from "./services/api";
-import UserAdd from "./users/add";
-import UserList from "./users/list";
+import Rotas from "./routes";
 
 function App() {
-  const [lista, setLista] = useState([]);
-
-  useEffect(() => {
-    api
-      .get(
-        "/users?access-token=74db48e291075450bdcbe60b248398f428968607c014bc0955b273cd025e3778"
-      )
-      .then((response) => setLista(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
-
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">ReactJS</Navbar.Brand>
-          <Nav>
-            <Nav.Link href="/">Lista dos Usuários</Nav.Link>
-            <Nav.Link href="/cadastro">Cadastro</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-      <Container>
-        <Row className="mb-5">
-          <Col>
-            <UserAdd />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <UserList usuarios={lista.data} />
-          </Col>
-        </Row>
-      </Container>
+      <Rotas />
     </>
   );
 }
