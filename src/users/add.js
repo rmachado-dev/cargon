0d3381a8ca84598f697c1bb78638fb0aa61bb6cb
Rogerio <rogerio.gordo@aparecida.go.gov.br>
2022-01-31 16:52:43 -0300

    teste de autenticação

--- src/users/add.js
+++ src/users/add.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React from "react";
-import { Button, Col, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { useForm } from "react-hook-form";
 
@@ -26,68 +26,69 @@ function UserAdd() {
 
   return (
     <>
-      <h1 className="py-5">Cadastro de Usuários</h1>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Row>
-          <Col sm={6} className="mb-3">
-            <Form.Label>Nome </Form.Label>
-            <Form.Control
-              type="text"
-              name="name"
-              {...register("name", { required: true })}
-            />
-            <small className="text-danger">
-              {errors.name?.type === "required" && "Campo obrigatório!"}
-            </small>
-          </Col>
-          <Col sm={6} className="mb-3">
-            <Form.Label>Email </Form.Label>
-            <Form.Control
-              type="email"
-              name="email"
-              {...register("email", { required: true })}
-            />
-            <small className="text-danger">
-              {errors.email?.type === "required" && "Campo obrigatório!"}
-            </small>
-          </Col>
-        </Row>
-        <Row className="py-sm-4">
-          <Col sm={6} className="mb-3">
-            <Form.Label>Gênero </Form.Label>
-            <Form.Select {...register("gender", { required: true })}>
-              <option value="">--</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </Form.Select>
+      <Container>
+        <h1 className="py-5">Cadastro de Usuários</h1>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Row>
+            <Col sm={6} className="mb-3">
+              <Form.Label>Nome </Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                {...register("name", { required: true })}
+              />
+              <small className="text-danger">
+                {errors.name?.type === "required" && "Campo obrigatório!"}
+              </small>
+            </Col>
+            <Col sm={6} className="mb-3">
+              <Form.Label>Email </Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                {...register("email", { required: true })}
+              />
+              <small className="text-danger">
+                {errors.email?.type === "required" && "Campo obrigatório!"}
+              </small>
+            </Col>
+          </Row>
+          <Row className="py-sm-4">
+            <Col sm={6} className="mb-3">
+              <Form.Label>Gênero </Form.Label>
+              <Form.Select {...register("gender", { required: true })}>
+                <option value="">--</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </Form.Select>
 
-            
-            <small className="text-danger">
-              {errors.gender?.type === "required" && "Campo obrigatório!"}
-            </small>
-          </Col>
+              <small className="text-danger">
+                {errors.gender?.type === "required" && "Campo obrigatório!"}
+              </small>
+            </Col>
 
-          <Col sm={6} className="mb-3">
-            <Form.Label>Status </Form.Label>
+            <Col sm={6} className="mb-3">
+              <Form.Label>Status </Form.Label>
 
-            <Form.Select {...register("status", { required: true })}>
-              <option value="">--</option>
-              <option value="active">Active</option>
-              <option value="inactive">Inactive</option>
-            </Form.Select>
-            <small className="text-danger">
-              {errors.status?.type === "required" && "Campo obrigatório!"}
-            </small>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button variant="primary" type="submit">
-              Cadastrar
-            </Button>
-          </Col>
-        </Row>
-      </Form>
+              <Form.Select {...register("status", { required: true })}>
+                <option value="">--</option>
+                <option value="active">Active</option>
+                <option value="inactive">Inactive</option>
+              </Form.Select>
+              <small className="text-danger">
+                {errors.status?.type === "required" && "Campo obrigatório!"}
+              </small>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button variant="primary" type="submit">
+                Cadastrar
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
     </>
   );
 }
